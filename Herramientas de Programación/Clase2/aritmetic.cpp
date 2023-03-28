3823ed191a4a86eaba37916f122d9f6f20b21c5f
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-03-28 00:46:51 -0300

    Create aritmetic.cpp

---
+++ Herramientas de Programación/Clase2/aritmetic.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+main(){
+	int dato1, dato2, resultado;
+	dato1 = 20;
+	dato2 = 10;
+	
+	// Suma
+	resultado = dato1 + dato2;
+	printf("%d + %d = %d\n", dato1, dato2, resultado);
+	
+	// Resta
+	resultado = dato1 - dato2;
+	printf("%d - %d = %d\n", dato1, dato2, resultado);
+	
+	// Producto
+	resultado = dato1 * dato2;
+	printf("%d * %d = %d\n", dato1, dato2, resultado);
+	
+	// Cociente
+	resultado = dato1 / dato2;
+	printf("%d / %d = %d\n", dato1, dato2, resultado);
+	
+	getchar();
+	return 0;
+}
