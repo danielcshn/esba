ef7b48cf951b55b70f6bb9f1f5df33409be5a7b7
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-03-28 01:00:49 -0300

    Create ejemplo2 (Errores)

---
+++ Herramientas de Programación/Clase2/ERRORES/ejemplo2-con-errores.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+main()
+
+{ int a, c; a = 5; c += a + 5; prnt("Elll valor de c= %d",c);}
---
+++ Herramientas de Programación/Clase2/ERRORES/ejemplo2-sin-errores.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+main() { 
+	int a, c;
+	a = 5;
+	c = a + 5;
+	printf("El valor de a = %d - El valor de c = %d", a, c);
+}
