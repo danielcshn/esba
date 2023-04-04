aa701077145de54119e89e132b84cf28419bd691
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-04-04 02:06:54 -0300

    Create enum1.cpp

---
+++ Herramientas de Programación/Clase3/enum1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	enum piso {
+		p0, // Valor 0
+		p1, // Valor 1
+		p2, // Valor 2
+		p3 // Valor 3
+		
+		/*
+		  (...)
+		*/
+	};
+	
+printf("La familia Perez vive en el piso %d \n", p1);
+printf("La familia Ramirez vive en el piso %d \n", p2);
+printf("La familia Gimenez vive en el piso %d \n", p3);
+
+/*
+  (...)
+*/
+
+system("PAUSE");
+return 0;
+}
