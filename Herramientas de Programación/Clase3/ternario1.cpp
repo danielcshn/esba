d7a7e304e6a3fe0d173f58ccc5e26e4e49ea47e0
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-04-04 02:48:06 -0300

    Create ternario1.cpp

---
+++ Herramientas de Programación/Clase3/ternario1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+	bool tr=true, fs=false;
+
+	string vr = "verdad";
+	string mt = "mentira";
+	string resultado;
+	
+	resultado = tr ? vr : mt;
+
+	//cout << resultado;
+	cout << resultado << endl;
+}
