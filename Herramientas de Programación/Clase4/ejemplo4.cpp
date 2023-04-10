3137a981b783480b74fc37f42e2ec7cf3d650f7e
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-04-10 19:01:26 -0300

    Create ejemplo4.cpp

---
+++ Herramientas de Programación/Clase4/ejemplo4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+main(){
+	char tex[2];
+	
+	printf("Ingrese una cadena de caracteres: \n");
+	scanf("%s", &tex);
+	
+	printf("El texto es: %s \n", tex);
+	system("PAUSE");
+}
