7a340512a6099034936b4c6aa4f7089a7e8229c5
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-04-10 18:46:47 -0300

    Create ejemplo2.cpp

---
+++ Herramientas de Programación/Clase4/ejemplo2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+main(){
+	char tex[2];
+	
+	printf("Ingrese un si o no: \n");
+	scanf("%s", &tex);
+	
+	printf("El texto es: %s \n", tex);
+	system("PAUSE");
+	
+}
