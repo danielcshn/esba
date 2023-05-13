7f703c52e6b5e88459722db4d8aba16f77c9f828
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-05-13 19:27:39 -0300

    Create TP2-2.cpp

---
+++ Herramientas de Programación/TP2/TP2-2.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <windows.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+///////////////////////////////////////////////////
+// Muestro nombres de Integrantes del Grupo
+
+nameDisplay(){
+	// Cambio color texto consola a verde
+	HANDLE hConsole;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 2);
+	// Muestro Integrantes
+	string integrantes[] = {"Matias X", "Juan X", "Fabian X", "Maximiliano X", "Geraldine X", "Daniel X", "Sergio X", "SergioX X", "Santiago X", "Veronica X"};
+	printf(" EQUIPO B N\370 2\n\n");
+	for (int i = 0; i < 10; i++){
+        cout << " - " << integrantes[i] << endl;
+    }
+    printf("\n\n");
+    // Cambio color texto consola a blanco
+    SetConsoleTextAttribute(hConsole, 15);
+}
+
+///////////////////////////////////////////////////
+// Ejercicio 2:
+// Teniendo en cuenta el ejemplo 1 de la clase FOR, realizar un programa que 
+// permita ingresar una cantidad X de ciclos utilizando scanf o cin 
+// (por ejemplo 20) y ver los números impares dentro de esa cantidad de números.
+
+int main(){
+	vuelve: 
+		nameDisplay();
+	
+		int ciclos;
+		string resp;
+		cout << "Ingrese una cantidad X de ciclos: " << endl;
+		cin >> ciclos;
+	
+		for(int i=0;i<=ciclos;i++){
+			if (i % 2 == 0) {
+				cout << i << " >>" << " es par" << endl;
+			} else {
+				cout << i << " >>" << " NO es par" << endl;
+			}
+		}
+
+		cout << "\n\nQuiere Ingresar otra cantidad de ciclos? (Responde s/si para si o n/no para no)" << endl;
+		cin >> resp;
+		if (resp == "s" || resp == "S" || resp == "Si" || resp == "SI") {
+			system("cls");
+			goto vuelve;
+		}
+
+	system("PAUSE");
+	cin.ignore();
+	return 0;
+}
