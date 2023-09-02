39efaad37aaecf176184c06fafa16ec8fc081fb3
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-09-02 00:18:05 -0300

    Create Hola.java

---
+++ Programación Orientada a Objetos/Clase 3/Hola/src/Hola.java
@@ -0,0 +1,12 @@
+
+public class Hola {
+	/** Clase Hola * v2023.09.02 * 02/09/2023 * Derechos Reservados */
+
+	public static void main(String[] args) {
+		System.out.println("Hola, ");
+		System.out.println("me llamo Daniel");
+		System.out.println("Hasta luego");
+
+	}
+
+}
