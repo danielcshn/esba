3f324ab906e14725de71c77c07e4844099ebebcb
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-09-02 00:33:47 -0300

    Create Actividad1

---
+++ Programación Orientada a Objetos/Clase 3/Actividad1/src/Actividad1.java
@@ -0,0 +1,16 @@
+
+public class Actividad1 {
+	// Clase Actividad1 * v2023.09.02 * 02/09/2023 * 
+	// 67213069+danielcshn@users.noreply.github.com * 
+
+	public static void main(String[] args) {
+
+		System.out.println("Programa 1");
+		System.out.println("Clase Actividad1");
+		System.out.println("v2023.09.02");
+		System.out.println("02/09/2023");
+		System.out.println("67213069+danielcshn@users.noreply.github.com");
+
+	}
+
+}
