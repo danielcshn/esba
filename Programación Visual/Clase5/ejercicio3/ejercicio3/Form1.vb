72b4363983efb5d23d91e3d0c376015ca108c75b
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-09-13 02:40:58 -0300

    Create ejercicio3

---
+++ Programación Visual/Clase5/ejercicio3/ejercicio3/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+		Dim A As Integer = 10
+		Dim B As Integer = 15
+		Dim C As Integer = 20
+		Dim D As Integer = 25
+
+		Console.WriteLine("Valor A: " & A & ".")
+		Console.WriteLine("Valor B: " & B & ".")
+		Console.WriteLine("Valor C: " & C & ".")
+		Console.WriteLine("Valor D: " & D & ".")
+
+		Dim tmpB As Integer = B
+		B = C
+		C = A
+		A = D
+		D = tmpB
+
+		Console.WriteLine("Valor A: " & A & ".")
+		Console.WriteLine("Valor B: " & B & ".")
+		Console.WriteLine("Valor C: " & C & ".")
+		Console.WriteLine("Valor D: " & D & ".")
+
+	End Sub
+End Class
