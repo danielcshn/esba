0f116ba3b95904c4f6d05145b523b510872fb689
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-09-13 02:34:17 -0300

    Create ejercicio1

---
+++ Programación Visual/Clase5/ejercicio1/ejercicio1/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim N As Integer = 10
+        Console.WriteLine("N: " & N)
+        N += 77
+        Console.WriteLine("Resultado es +77: " & N)
+        N -= 3
+        Console.WriteLine("Resultado es -3: " & N)
+        N *= 2
+        Console.WriteLine("Resultado es X2: " & N)
+
+    End Sub
+End Class
