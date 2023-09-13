93d15d5c2e21f96fe322cb728ac6823ca1087bef
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-09-13 02:43:40 -0300

    Create ejercicio4

---
+++ Programación Visual/Clase5/ejercicio4/ejercicio4/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim A As Integer = 15
+
+        If A Mod 2 = 0 Then
+            Console.WriteLine("El valor de A (" & A & ") es par.")
+        Else
+            Console.WriteLine("El valor de A (" & A & ") es impar.")
+        End If
+
+    End Sub
+End Class
