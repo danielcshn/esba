985fc6779bb5b15e657f19c8be048ebba318c730
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-09-13 02:46:03 -0300

    Create ejercicio5

---
+++ Programación Visual/Clase5/ejercicio5/ejercicio5/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim B As Integer = -10
+
+        If B >= 0 Then
+            Console.WriteLine("El valor de B es positivo o cero.")
+        Else
+            Console.WriteLine("El valor de B es negativo.")
+        End If
+
+    End Sub
+End Class
