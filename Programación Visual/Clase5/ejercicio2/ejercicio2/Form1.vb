701ee7afd0126aa3aa425cbd40d2627bbcba40a8
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-09-13 02:37:56 -0300

    Create ejercicio2

---
+++ Programación Visual/Clase5/ejercicio2/ejercicio2/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim var1 As Integer = 10
+        Dim var2 As Integer = 20
+
+        If var1 > var2 Then
+            Console.WriteLine("El primer número (" & var1 & ") es mayor que el segundo número (" & var2 & ").")
+        ElseIf var1 < var2 Then
+            Console.WriteLine("El primer número (" & var1 & ") es menor que el segundo número (" & var2 & ").")
+        Else
+            Console.WriteLine("Ambos números son iguales.")
+        End If
+
+    End Sub
+End Class
---
+++ Programación Visual/Clase5/ejercicio2/ejercicio2/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("ejercicio2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ejercicio2")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("64171889-cf75-4b4c-a7e5-0c1f15ab14ff")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
