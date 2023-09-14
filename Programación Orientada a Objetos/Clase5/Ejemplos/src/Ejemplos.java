5f692407438fb91d37e69271710fe4a8ad5e65f2
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-09-14 19:55:52 -0300

    Update Ejemplos.java

--- Programación Orientada a Objetos/Clase5/Ejemplos/src/Ejemplos.java
+++ Programación Orientada a Objetos/Clase5/Ejemplos/src/Ejemplos.java
@@ -1,4 +1,7 @@
 
+import javax.swing.JOptionPane;
+
+
 public class Ejemplos {
 
 	public static void main(String[] args) {
@@ -6,9 +9,12 @@ public class Ejemplos {
 		int precioProducto=300;
 		final double IVA=0.21;
 		
-		System.out.println("Información del producto");
-		System.out.println("El precio del producto es "+precioProducto);
-		System.out.println("El precio del producto, incluyendo el IVA, es "+(precioProducto+(precioProducto*IVA)));
+		// System.out.println("Información del producto");
+		// System.out.println("El precio del producto es "+precioProducto);
+		// System.out.println("El precio del producto, incluyendo el IVA, es "+(precioProducto+(precioProducto*IVA)));
+		
+		JOptionPane.showMessageDialog(null, "El precio del producto, Incluido el IVA es "+(precioProducto+(precioProducto*IVA)));
+
 		
 	}
 
