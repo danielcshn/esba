aefb5fbf5df2d0e5e82128ae44fa2a55be32b17a
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-09-14 19:58:59 -0300

    Create Ejemplo

---
+++ Programación Orientada a Objetos/Clase5/Ejemplo/src/Ejemplo.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre=JOptionPane.showInputDialog("Introduce tu nombre");
+		JOptionPane.showMessageDialog(null, "Bien, tu nombre es "+nombre);
+	}
+
+}
