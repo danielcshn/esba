6936e83673f8c5d8e849655863574dcb7b8fc199
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-09-19 23:21:32 -0300

    Create actividad1

---
+++ Programación Visual/Clase6/actividad1/actividad1/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDate1 = New System.Windows.Forms.TextBox()
+        Me.txtDate2 = New System.Windows.Forms.TextBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.lbResultado = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(186, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingrese la primera fecha (yyyy-mm-dd):"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(196, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Ingrese la segunda fecha (yyyy-mm-dd): "
+        '
+        'txtDate1
+        '
+        Me.txtDate1.Location = New System.Drawing.Point(12, 25)
+        Me.txtDate1.Name = "txtDate1"
+        Me.txtDate1.Size = New System.Drawing.Size(204, 20)
+        Me.txtDate1.TabIndex = 2
+        Me.txtDate1.Text = "2020-07-10"
+        '
+        'txtDate2
+        '
+        Me.txtDate2.Location = New System.Drawing.Point(12, 64)
+        Me.txtDate2.Name = "txtDate2"
+        Me.txtDate2.Size = New System.Drawing.Size(204, 20)
+        Me.txtDate2.TabIndex = 3
+        Me.txtDate2.Text = "2022-07-10"
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(12, 90)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(204, 23)
+        Me.btnCalcular.TabIndex = 4
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'lbResultado
+        '
+        Me.lbResultado.AutoSize = True
+        Me.lbResultado.Location = New System.Drawing.Point(12, 116)
+        Me.lbResultado.Name = "lbResultado"
+        Me.lbResultado.Size = New System.Drawing.Size(0, 13)
+        Me.lbResultado.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(228, 180)
+        Me.Controls.Add(Me.lbResultado)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.txtDate2)
+        Me.Controls.Add(Me.txtDate1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtDate1 As TextBox
+    Friend WithEvents txtDate2 As TextBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents lbResultado As Label
+End Class
---
+++ Programación Visual/Clase6/actividad1/actividad1/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim fecha1 As Date = Date.Parse(txtDate1.Text)
+        Dim fecha2 As Date = Date.Parse(txtDate2.Text)
+
+        ' Calcular la diferencia en días
+        Dim diferenciaDias As Integer = (fecha2 - fecha1).Days
+
+        ' Calcular la diferencia en semanas
+        Dim diferenciaSemanas As Integer = diferenciaDias \ 7
+
+        ' Calcular la diferencia en meses
+        Dim diferenciaMeses As Integer = Math.Abs((fecha2.Year - fecha1.Year) * 12 + fecha2.Month - fecha1.Month)
+
+        ' Calcular la diferencia en años
+        Dim diferenciaAnios As Integer = Math.Abs(fecha2.Year - fecha1.Year)
+
+        lbResultado.Text = "Diferencia en días: " & diferenciaDias & " días" & vbCrLf &
+            "Diferencia en semanas: " & diferenciaSemanas & " semanas" & vbCrLf &
+            "Diferencia en meses: " & diferenciaMeses & " meses" & vbCrLf &
+            "Diferencia en años: " & diferenciaAnios & " años"
+
+    End Sub
+End Class
