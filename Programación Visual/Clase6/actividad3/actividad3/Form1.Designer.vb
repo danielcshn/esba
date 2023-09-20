333bd38b60652a9f66567c239887836a14dbecc9
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-09-20 00:35:59 -0300

    Create actividad3

---
+++ Programación Visual/Clase6/actividad3/actividad3/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtEdad = New System.Windows.Forms.TextBox()
+        Me.txtSexo = New System.Windows.Forms.TextBox()
+        Me.btnValidar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "nombre:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "edad:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(14, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(32, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "sexo:"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(15, 25)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 3
+        '
+        'txtEdad
+        '
+        Me.txtEdad.Location = New System.Drawing.Point(15, 64)
+        Me.txtEdad.Name = "txtEdad"
+        Me.txtEdad.Size = New System.Drawing.Size(100, 20)
+        Me.txtEdad.TabIndex = 4
+        '
+        'txtSexo
+        '
+        Me.txtSexo.Location = New System.Drawing.Point(17, 103)
+        Me.txtSexo.Name = "txtSexo"
+        Me.txtSexo.Size = New System.Drawing.Size(100, 20)
+        Me.txtSexo.TabIndex = 5
+        '
+        'btnValidar
+        '
+        Me.btnValidar.Location = New System.Drawing.Point(15, 129)
+        Me.btnValidar.Name = "btnValidar"
+        Me.btnValidar.Size = New System.Drawing.Size(102, 23)
+        Me.btnValidar.TabIndex = 6
+        Me.btnValidar.Text = "validar"
+        Me.btnValidar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(133, 169)
+        Me.Controls.Add(Me.btnValidar)
+        Me.Controls.Add(Me.txtSexo)
+        Me.Controls.Add(Me.txtEdad)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtEdad As TextBox
+    Friend WithEvents txtSexo As TextBox
+    Friend WithEvents btnValidar As Button
+End Class
---
+++ Programación Visual/Clase6/actividad3/actividad3/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub btnValidar_Click(sender As Object, e As EventArgs) Handles btnValidar.Click
+        Dim nombre As String = txtNombre.Text
+        Dim edad As Integer
+        Dim sexo As Char
+
+        ' Verificar el nombre
+        If nombre.Any(Function(c) Char.IsDigit(c)) Then
+            MessageBox.Show("El nombre no debe contener números.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Verificar la edad
+        If Not Integer.TryParse(txtEdad.Text, edad) Then
+            MessageBox.Show("La edad debe ser un número.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Verificar el sexo
+        sexo = Char.ToUpper(txtSexo.Text.Trim())
+        If sexo <> "F"c AndAlso sexo <> "M"c Then
+            MessageBox.Show("El sexo debe ser 'F' o 'M'.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Mostrar los datos ingresados
+        MessageBox.Show("Datos ingresados:" & vbCrLf &
+                        "Nombre: " & nombre & vbCrLf &
+                        "Edad: " & edad & vbCrLf &
+                        "Sexo: " & sexo, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
