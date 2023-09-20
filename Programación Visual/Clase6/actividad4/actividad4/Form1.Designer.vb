77ce27532ad761108430b39aa000f6850909b831
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-09-20 00:41:22 -0300

    Create actividad4

---
+++ Programación Visual/Clase6/actividad4/actividad4/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnVerificar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(15, 25)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum1.TabIndex = 0
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(121, 25)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "numero1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(118, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "numero2"
+        '
+        'btnVerificar
+        '
+        Me.btnVerificar.Location = New System.Drawing.Point(15, 51)
+        Me.btnVerificar.Name = "btnVerificar"
+        Me.btnVerificar.Size = New System.Drawing.Size(206, 23)
+        Me.btnVerificar.TabIndex = 4
+        Me.btnVerificar.Text = "verificar"
+        Me.btnVerificar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(238, 92)
+        Me.Controls.Add(Me.btnVerificar)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents txtNum2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnVerificar As Button
+End Class
---
+++ Programación Visual/Clase6/actividad4/actividad4/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub btnVerificar_Click(sender As Object, e As EventArgs) Handles btnVerificar.Click
+        ' Verificar si ambos TextBox tienen valores ingresados
+        If String.IsNullOrWhiteSpace(txtNum1.Text) Or String.IsNullOrWhiteSpace(txtNum2.Text) Then
+            MessageBox.Show("Por favor, ingrese ambos números.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Convertir los valores de los TextBox a números
+        Dim numero1 As Double
+        Dim numero2 As Double
+
+        If Not Double.TryParse(txtNum1.Text, numero1) Or Not Double.TryParse(txtNum2.Text, numero2) Then
+            MessageBox.Show("Por favor, ingrese números válidos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Comparar los números
+        If numero1 > numero2 Then
+            MessageBox.Show("El número " & numero1.ToString() & " es mayor que " & numero2.ToString(), "Resultado")
+        ElseIf numero1 < numero2 Then
+            MessageBox.Show("El número " & numero2.ToString() & " es mayor que " & numero1.ToString(), "Resultado")
+        Else
+            MessageBox.Show("Ambos números son iguales: " & numero1.ToString(), "Resultado")
+        End If
+    End Sub
+End Class
