1849ce6796ccd0b9d6a998283448ca2236348996
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-09-20 00:26:30 -0300

    Create actividad2

---
+++ Programación Visual/Clase6/actividad2/actividad2/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnRepresenta = New System.Windows.Forms.Button()
+        Me.txtCodigo = New System.Windows.Forms.TextBox()
+        Me.lbResultado = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(219, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingrese el código del alumno (10 caracteres):"
+        '
+        'btnRepresenta
+        '
+        Me.btnRepresenta.Location = New System.Drawing.Point(15, 51)
+        Me.btnRepresenta.Name = "btnRepresenta"
+        Me.btnRepresenta.Size = New System.Drawing.Size(75, 23)
+        Me.btnRepresenta.TabIndex = 1
+        Me.btnRepresenta.Text = "Ver"
+        Me.btnRepresenta.UseVisualStyleBackColor = True
+        '
+        'txtCodigo
+        '
+        Me.txtCodigo.Location = New System.Drawing.Point(15, 25)
+        Me.txtCodigo.MaxLength = 10
+        Me.txtCodigo.Name = "txtCodigo"
+        Me.txtCodigo.Size = New System.Drawing.Size(108, 20)
+        Me.txtCodigo.TabIndex = 2
+        Me.txtCodigo.Text = "20153CXJ91"
+        '
+        'lbResultado
+        '
+        Me.lbResultado.AutoSize = True
+        Me.lbResultado.Location = New System.Drawing.Point(12, 77)
+        Me.lbResultado.Name = "lbResultado"
+        Me.lbResultado.Size = New System.Drawing.Size(0, 13)
+        Me.lbResultado.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(130, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "ej. 20152CI1B2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(241, 169)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lbResultado)
+        Me.Controls.Add(Me.txtCodigo)
+        Me.Controls.Add(Me.btnRepresenta)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnRepresenta As Button
+    Friend WithEvents txtCodigo As TextBox
+    Friend WithEvents lbResultado As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Programación Visual/Clase6/actividad2/actividad2/Form1.vb
@@ -0,0 +1,64 @@
+﻿Public Class Form1
+
+    Dim resultadoFinal As String = ""
+
+    Private Sub btnRepresenta_Click(sender As Object, e As EventArgs) Handles btnRepresenta.Click
+
+        Dim codigoAlumno As String = txtCodigo.Text
+
+        ' Verificar si el código tiene 10 caracteres
+        If codigoAlumno.Length = 10 Then
+            ' Obtener los componentes del código
+            Dim anioMatricula As String = codigoAlumno.Substring(0, 4)
+            Dim semestre As Char = codigoAlumno(4)
+            Dim especialidad As String = codigoAlumno.Substring(5, 2)
+            Dim ciclo As Char = codigoAlumno(7)
+            Dim turno As Char = codigoAlumno(8)
+            Dim grupo As Char = codigoAlumno(9)
+
+            resultadoFinal = "Año de matrícula: " & anioMatricula & vbCrLf
+
+            If semestre = "1" Then
+                resultadoFinal &= "Semestre Académico: 1" & vbCrLf
+            ElseIf semestre = "2" Then
+                resultadoFinal &= "Semestre Académico: 2" & vbCrLf
+            Else
+                resultadoFinal &= "Semestre Académico: Error" & vbCrLf
+            End If
+
+            Select Case especialidad
+                Case "CI"
+                    resultadoFinal &= "Especialidad: Computación" & vbCrLf
+                Case "CO"
+                    resultadoFinal &= "Especialidad: Contabilidad" & vbCrLf
+                Case "SE"
+                    resultadoFinal &= "Especialidad: Secretariado" & vbCrLf
+                Case "ET"
+                    resultadoFinal &= "Especialidad: Enfermería" & vbCrLf
+                Case Else
+                    resultadoFinal &= "Especialidad: Error" & vbCrLf
+            End Select
+
+            If Char.IsDigit(ciclo) Then
+                resultadoFinal &= "Ciclo: " & ciclo & vbCrLf
+            Else
+                resultadoFinal &= "Ciclo: Error" & vbCrLf
+            End If
+
+            If Char.IsLetter(turno) Then
+                resultadoFinal &= "Turno: " & turno & vbCrLf
+            Else
+                resultadoFinal &= "Turno: Error" & vbCrLf
+            End If
+
+            resultadoFinal &= "Grupo: " & grupo & vbCrLf
+
+            lbResultado.Text = resultadoFinal
+
+        Else
+            lbResultado.Text = "Error: El código debe tener 10 caracteres."
+        End If
+
+    End Sub
+
+End Class
