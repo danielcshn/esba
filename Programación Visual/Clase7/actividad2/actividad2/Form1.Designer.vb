3e619a32aa12afef89178d81f6dc529979eb2bde
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-09-26 23:06:11 -0300

    Create actividad2

---
+++ Programación Visual/Clase7/actividad2/actividad2/Form1.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtNota1 = New System.Windows.Forms.TextBox()
+        Me.txtNota2 = New System.Windows.Forms.TextBox()
+        Me.txtNota3 = New System.Windows.Forms.TextBox()
+        Me.txtNota4 = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "NOTA 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "NOTA 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(119, 48)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "NOTA 3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(119, 87)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(46, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "NOTA 4"
+        '
+        'txtNota1
+        '
+        Me.txtNota1.Location = New System.Drawing.Point(15, 64)
+        Me.txtNota1.Name = "txtNota1"
+        Me.txtNota1.Size = New System.Drawing.Size(100, 20)
+        Me.txtNota1.TabIndex = 2
+        '
+        'txtNota2
+        '
+        Me.txtNota2.Location = New System.Drawing.Point(15, 103)
+        Me.txtNota2.Name = "txtNota2"
+        Me.txtNota2.Size = New System.Drawing.Size(100, 20)
+        Me.txtNota2.TabIndex = 3
+        '
+        'txtNota3
+        '
+        Me.txtNota3.Location = New System.Drawing.Point(122, 64)
+        Me.txtNota3.Name = "txtNota3"
+        Me.txtNota3.Size = New System.Drawing.Size(100, 20)
+        Me.txtNota3.TabIndex = 4
+        '
+        'txtNota4
+        '
+        Me.txtNota4.Location = New System.Drawing.Point(122, 103)
+        Me.txtNota4.Name = "txtNota4"
+        Me.txtNota4.Size = New System.Drawing.Size(100, 20)
+        Me.txtNota4.TabIndex = 5
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(15, 25)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(207, 20)
+        Me.txtNombre.TabIndex = 1
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(143, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "nombre y apellido del alumno"
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(122, 129)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(100, 23)
+        Me.btnCalcular.TabIndex = 6
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(237, 169)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.txtNota4)
+        Me.Controls.Add(Me.txtNota3)
+        Me.Controls.Add(Me.txtNota2)
+        Me.Controls.Add(Me.txtNota1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Promedio y Puntación"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtNota1 As TextBox
+    Friend WithEvents txtNota2 As TextBox
+    Friend WithEvents txtNota3 As TextBox
+    Friend WithEvents txtNota4 As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnCalcular As Button
+End Class
