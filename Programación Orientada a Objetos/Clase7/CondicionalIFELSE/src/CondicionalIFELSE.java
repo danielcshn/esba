d1503932e46c40abb9d53699e7a9a40043395c0a
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-10-16 13:34:23 -0300

    Create CondicionalIFELSE

---
+++ Programación Orientada a Objetos/Clase7/CondicionalIFELSE/src/CondicionalIFELSE.java
@@ -0,0 +1,17 @@
+
+public class CondicionalIFELSE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int precio=50;
+		
+		if (precio>100) {
+			System.out.println("El precio es mayor que 100");
+		} else {
+			System.out.println("El precio es menor que 100");
+		}
+		
+	}
+
+}
