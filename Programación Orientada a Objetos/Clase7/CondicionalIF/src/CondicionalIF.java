ab2dd30c83645bcc112258ab2cae91901c40463b
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-10-16 13:31:25 -0300

    Create CondicionalIF

---
+++ Programación Orientada a Objetos/Clase7/CondicionalIF/src/CondicionalIF.java
@@ -0,0 +1,15 @@
+
+public class CondicionalIF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int precio=300;
+		
+		if (precio>100) {
+			System.out.println("El precio es mayor que 100");
+		}
+		
+	}
+
+}
