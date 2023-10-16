bcaece155c440863e2604fc6fbffed62a24b5cec
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-10-16 15:34:40 -0300

    Create bucles

---
+++ Programación Orientada a Objetos/Clase7/bucles/src/bucles/bucles.java
@@ -0,0 +1,17 @@
+package bucles;
+
+public class bucles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num=1;
+		
+		while(num<=5) {
+			System.out.println(num);
+			num++;
+		}
+
+	}
+
+}
