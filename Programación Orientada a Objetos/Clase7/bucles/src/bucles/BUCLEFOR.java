be677e6473e5731c3d48117d1241b129449a89df
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-10-16 15:37:28 -0300

    Create BUCLEFOR.java

---
+++ Programación Orientada a Objetos/Clase7/bucles/src/bucles/BUCLEFOR.java
@@ -0,0 +1,14 @@
+package bucles;
+
+public class BUCLEFOR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int num=1; num<=5; num++) {
+			System.out.println(num);
+		}
+		
+	}
+
+}
