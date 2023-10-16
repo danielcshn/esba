13010a5395fea91938874877f2370b8163c4f3cd
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-10-16 13:46:24 -0300

    Create IFELSEIF

---
+++ Programación Orientada a Objetos/Clase7/IFELSEIF/src/IFELSEIF.java
@@ -0,0 +1,19 @@
+
+public class IFELSEIF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int precio=50;
+		
+		if (precio>100) {
+			System.out.println("El precio es mayor que 100");
+		} else if(precio>80) {
+			System.out.println("El precio es mayor que 80");
+		} else {
+			System.out.println("El precio es menor que 80");
+		}
+		
+	}
+
+}
