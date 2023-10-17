4e15b803986121819e06f3a9def480442bebabc1
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-10-16 21:11:24 -0300

    Update Vehiculo.java

--- Programación Orientada a Objetos/Clase8/Ejemplo/src/Vehiculo.java
+++ Programación Orientada a Objetos/Clase8/Ejemplo/src/Vehiculo.java
@@ -6,8 +6,15 @@ public class Vehiculo {
 		verde=2,
 		azul=3;
 	
+	public static final String[] nombre= {"error","rojo","verde","azul"};
+	
 	private int color;
 	private int numSerie;
+	
+	public Vehiculo(int color, int numSerie) {
+		this.color = color;
+		this.numSerie = numSerie;
+	}
 	
 	public int numSerie(){
 		return numSerie;
@@ -23,6 +30,10 @@ public class Vehiculo {
 	
 	public void pinta(int nuevoColor) {
 		color = nuevoColor;
+	}
+	
+	public String toString() {
+		return "Vehiculo -> numSerie= " + numSerie + ", color= "+ nombre[color];
 	}
 	
 }
