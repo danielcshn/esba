4369afcdb11d4e89e6fb104c96907ce00a24f47f
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-10-16 21:11:32 -0300

    Update Coche.java

--- Programación Orientada a Objetos/Clase8/Ejemplo/src/Coche.java
+++ Programación Orientada a Objetos/Clase8/Ejemplo/src/Coche.java
@@ -3,12 +3,21 @@ public class Coche extends Vehiculo {
 
 	private int cilindrada;
 	
+	public Coche(int color, int numSerie, int cilindrada) {
+		super(color, numSerie);
+		this.cilindrada = cilindrada;
+	}
+	
 	public int cilindrada() {
 		return cilindrada;
 	}
 	
 	public void cambiaCilindrada(int nuevaCilin) {
 		this.cilindrada = nuevaCilin;
+	}
+	
+	public String toString() {
+		return super.toString() + ", cilindrada= "+ cilindrada;
 	}
 	
 }
