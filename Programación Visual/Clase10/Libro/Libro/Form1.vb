47f0058f145643e2966baabe70735e63b84522f4
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-10-17 20:53:46 -0300

    Create Libro

---
+++ Programación Visual/Clase10/Libro/Libro/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Ejemplo de cómo utilizar la clase Libro
+        Dim miLibro As New Libro("Introduction to Programming", "Liang, Y. Daniel", "0-13-031997-X", 784, "3a", "Prentice-Hall", "Nueva Jersey", "USA", New DateTime(2001, 11, 16))
+        miLibro.LeerInformacion()
+    End Sub
+
+End Class
