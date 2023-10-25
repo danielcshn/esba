2aca95638a750ff77fefd144c736ce85d8c17ac7
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-10-24 21:41:20 -0300

    Create Empleado

---
+++ Programación Visual/Clase10/Empleado/Empleado/Empleado.vb
@@ -0,0 +1,75 @@
+﻿Public Class Empleado
+
+    Private nombreEmpleado As String
+    Private apellidoEmpleado As String
+    Public activo As Boolean
+
+    Public Property nombre() As String
+        Get
+            nombre = nombreEmpleado
+        End Get
+        Set(nom As String)
+            nombreEmpleado = nom
+        End Set
+    End Property
+
+    Public Property apellido() As String
+        Get
+            Return apellidoEmpleado
+        End Get
+        Set(ByVal apell As String)
+            apellidoEmpleado = apell
+        End Set
+    End Property
+
+    Private numeroLegajo As Integer
+
+    ReadOnly Property legajo() As Integer
+        Get
+            legajo = numeroLegajo
+        End Get
+    End Property
+
+    Private valorContraseña As String
+    WriteOnly Property contraseña() As String
+        Set(ByVal value As String)
+            valorContraseña = value
+        End Set
+    End Property
+
+    Private añoIngresoEmpleado As Integer
+
+    WriteOnly Property añoIngreso() As Integer
+        Set(ByVal año As Integer)
+            añoIngresoEmpleado = año
+        End Set
+    End Property
+
+    ReadOnly Property antiguedad() As String
+        Get
+            antiguedad = CalcAntiguedad(añoIngresoEmpleado)
+        End Get
+    End Property
+
+    Public Function apellido_nombre() As String
+        If (nombreEmpleado <> "") And (apellidoEmpleado <> "") Then
+            apellido_nombre = nombreEmpleado & " " & apellidoEmpleado
+        Else
+            MsgBox("Debe cargar nombre y apellido del empleado")
+            apellido_nombre = ""
+        End If
+    End Function
+
+    Private Function CalcAntiguedad(ByVal año As Integer) As Integer
+        CalcAntiguedad = My.Computer.Clock.LocalTime.Year - año
+    End Function
+
+    Public Function inicialApellido() As String
+        inicialApellido = Left$(apellidoEmpleado, 1)
+    End Function
+
+    Public Function inicialApellido(ByVal punto As Boolean) As String
+        inicialApellido = Left$(apellidoEmpleado, 1) & "."
+    End Function
+
+End Class
---
+++ Programación Visual/Clase10/Empleado/Empleado/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+
+    Dim Empleado1 As New Empleado
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        With Empleado1
+            .nombre = TextBox2.Text
+            .apellido = TextBox3.Text
+            .añoIngreso = TextBox4.Text
+            .activo = CheckBox1.Checked
+        End With
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox(Empleado1.apellido_nombre)
+
+        MsgBox(CStr(Empleado1.antiguedad) & " años ")
+
+        If Empleado1.activo = True Then
+            MsgBox(Empleado1.apellido & " esta activo")
+        Else
+            MsgBox(Empleado1.apellido & " fue dado de baja")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MsgBox(Empleado1.nombre & Empleado1.inicialApellido(True))
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        MsgBox(Empleado1.nombre & UCase(Empleado1.inicialApellido))
+    End Sub
+End Class
