b4f62b4e80ef97b7f453ae927d5a3c20b9aba2bb
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-11-08 00:44:17 -0300

    Update UsuarioNombre.vb

--- Programación Visual/Clase12/Ejemplo/ControlUsuario/ControlUsuario/UsuarioNombre.vb
+++ Programación Visual/Clase12/Ejemplo/ControlUsuario/ControlUsuario/UsuarioNombre.vb
@@ -1,6 +1,40 @@
 ﻿Public Class UsuarioNombre
+
+    Property primerNombreText() As String
+        Get
+            Return primerNombre.Text
+        End Get
+        Set(ByVal value As String)
+            primerNombre.Text = value
+        End Set
+    End Property
+    Property segundoNombreText() As String
+        Get
+            Return segundoNombre.Text
+        End Get
+        Set(ByVal value As String)
+            segundoNombre.Text = value
+        End Set
+    End Property
+    Property apellidoText() As String
+        Get
+            Return apellido.Text
+        End Get
+        Set(ByVal value As String)
+            apellido.Text = value
+        End Set
+    End Property
+
+    ReadOnly Property Apellido_NombresText() As String
+        Get
+            Return Apellido_Nombres.Text
+        End Get
+    End Property
+
     Private Sub primerNombre_TextChanged(sender As Object, e As EventArgs) Handles primerNombre.TextChanged, segundoNombre.TextChanged, apellido.TextChanged
         ' Muestra el contenido de las cajas de textos en la etiqueta.
         Apellido_Nombres.Text = primerNombre.Text & " " & segundoNombre.Text & " " & apellido.Text
     End Sub
+
+
 End Class
