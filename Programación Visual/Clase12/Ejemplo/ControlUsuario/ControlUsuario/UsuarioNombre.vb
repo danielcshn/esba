558269374574d29ff991f6eac41fedb4274ddbf8
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-11-08 00:48:55 -0300

    Update UsuarioNombre.vb

--- Programación Visual/Clase12/Ejemplo/ControlUsuario/ControlUsuario/UsuarioNombre.vb
+++ Programación Visual/Clase12/Ejemplo/ControlUsuario/ControlUsuario/UsuarioNombre.vb
@@ -1,5 +1,22 @@
 ﻿Public Class UsuarioNombre
 
+    Private ListaMostrarEstilo As Mostrar
+    Property MostrarEstilo() As Mostrar
+        Get
+            Return ListaMostrarEstilo
+        End Get
+        Set(ByVal valor As Mostrar)
+            ListaMostrarEstilo = valor
+        End Set
+    End Property
+
+    Public Enum Mostrar
+        primerNombresegundoNombreapellido
+        primeraNombreapellido
+        apellidoprimerNombresegundoNombre
+        apellidoprimerNombre
+    End Enum
+
     Property primerNombreText() As String
         Get
             Return primerNombre.Text
@@ -8,6 +25,7 @@
             primerNombre.Text = value
         End Set
     End Property
+
     Property segundoNombreText() As String
         Get
             Return segundoNombre.Text
@@ -16,6 +34,7 @@
             segundoNombre.Text = value
         End Set
     End Property
+
     Property apellidoText() As String
         Get
             Return apellido.Text
@@ -33,7 +52,23 @@
 
     Private Sub primerNombre_TextChanged(sender As Object, e As EventArgs) Handles primerNombre.TextChanged, segundoNombre.TextChanged, apellido.TextChanged
         ' Muestra el contenido de las cajas de textos en la etiqueta.
-        Apellido_Nombres.Text = primerNombre.Text & " " & segundoNombre.Text & " " & apellido.Text
+        ' Apellido_Nombres.Text = primerNombre.Text & " " & segundoNombre.Text & " " & apellido.Text
+
+        Select Case ListaMostrarEstilo
+            Case Mostrar.primeraNombreapellido
+                Apellido_Nombres.Text = primerNombre.Text & " " &
+               apellido.Text
+            Case Mostrar.primerNombresegundoNombreapellido
+                Apellido_Nombres.Text = primerNombre.Text & " " &
+               segundoNombre.Text & " " & apellido.Text
+            Case Mostrar.apellidoprimerNombre
+                Apellido_Nombres.Text = apellido.Text & ", " &
+               primerNombre.Text
+            Case Mostrar.apellidoprimerNombresegundoNombre
+                Apellido_Nombres.Text = apellido.Text & ", " &
+               primerNombre.Text & " " & segundoNombre.Text
+        End Select
+
     End Sub
 
 
