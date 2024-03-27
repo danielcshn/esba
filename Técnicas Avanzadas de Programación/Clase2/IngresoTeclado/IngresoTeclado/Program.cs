7c0fee4ae452ddf134c610e4fdd54f0d6abd8a5f
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2024-03-27 02:10:59 -0300

    Create IngresoTeclado

---
+++ Técnicas Avanzadas de Programación/Clase2/IngresoTeclado/IngresoTeclado/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IngresoTeclado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese Nombre: ");
+            string strNombre;
+            strNombre = Console.ReadLine();
+            Console.WriteLine("Usted Ingreso: " + strNombre);
+        }
+    }
+}
