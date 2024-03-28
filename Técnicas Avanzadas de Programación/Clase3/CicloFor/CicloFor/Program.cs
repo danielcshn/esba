5c4db78c84174fbbb6c638a9f96b36b2422f01e5
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2024-03-28 19:09:10 -0300

    Create CicloFor

---
+++ Técnicas Avanzadas de Programación/Clase3/CicloFor/CicloFor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CicloFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Note: "i" es solo visible dentro del ámbito del ciclo for.
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Numero: {0} ", i);
+            }
+            // "i" no es visible aquí.
+        }
+    }
+}
