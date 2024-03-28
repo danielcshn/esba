2cf3606308a1fd38c9764f3908a4968ae38ee8e6
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2024-03-28 19:10:51 -0300

    Create CicloForeach

---
+++ Técnicas Avanzadas de Programación/Clase3/CicloForeach/CicloForeach/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CicloForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] carTypes = { "Ford", "BMW", "Mazda", "Honda" };
+            foreach (string c in carTypes)
+                Console.WriteLine(c);
+
+            int[] myInts = { 10, 20, 30, 40 };
+
+            foreach (int i in myInts)
+                Console.WriteLine(i);
+        }
+    }
+}
