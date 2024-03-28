c9f8fb921a7b292b32380be8c51afd90562888d8
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2024-03-28 19:13:42 -0300

    Create CicloDoWhile

---
+++ Técnicas Avanzadas de Programación/Clase3/CicloDoWhile/CicloDoWhile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CicloDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userIsDone = "";
+            do
+            {
+                Console.WriteLine("Dentro de un ciclo do/while");
+                Console.Write("¿Termina? [si] [no]: ");
+                userIsDone = Console.ReadLine();
+            }
+            while (userIsDone.ToLower() != "si"); // Note el punto y coma!!
+        }
+    }
+}
