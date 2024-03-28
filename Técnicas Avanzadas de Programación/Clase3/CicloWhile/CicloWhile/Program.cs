c1b8308dff471f16739b4d5b0663f75595282780
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2024-03-28 19:12:21 -0300

    Create CicloWhile

---
+++ Técnicas Avanzadas de Programación/Clase3/CicloWhile/CicloWhile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CicloWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userIsDone = "";
+            // Atención: el ciclo checkea "si" en minúsculas
+            while (userIsDone.ToLower() != "si")
+            {
+                Console.WriteLine("Dentro del ciclo while");
+                Console.Write("¿Termina? [si] [no]: ");
+                userIsDone = Console.ReadLine();
+            }
+        }
+    }
+}
