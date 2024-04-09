e3c8415383b334df57b81c87d8f485728efa09e3
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2024-04-09 00:34:28 -0300

    Actividad1

---
+++ Técnicas Avanzadas de Programación/Clase5/Actividad1/Actividad1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Actividad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arreglo = new int[10];
+
+            // Cargar el arreglo
+            Console.WriteLine("Ingrese 10 números enteros:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Elemento {i + 1}: ");
+                arreglo[i] = int.Parse(Console.ReadLine());
+            }
+
+            // 1.1 Averiguar el acumulado del arreglo
+            int acumulado = 0;
+            foreach (int num in arreglo)
+            {
+                acumulado += num;
+            }
+            Console.WriteLine($"Acumulado del arreglo: {acumulado}");
+
+            // 1.2 Encontrar el elemento más grande
+            int maximo = arreglo[0];
+            foreach (int num in arreglo)
+            {
+                if (num > maximo)
+                {
+                    maximo = num;
+                }
+            }
+            Console.WriteLine($"Elemento más grande: {maximo}");
+
+            // 1.3 Encontrar el elemento más chico
+            int minimo = arreglo[0];
+            foreach (int num in arreglo)
+            {
+                if (num < minimo)
+                {
+                    minimo = num;
+                }
+            }
+            Console.WriteLine($"Elemento más chico: {minimo}");
+
+            // 1.4 Imprimir el arreglo desde el último elemento al primero
+            Console.WriteLine("Arreglo en orden inverso:");
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(arreglo[i]);
+            }
+        }
+    }
+}
