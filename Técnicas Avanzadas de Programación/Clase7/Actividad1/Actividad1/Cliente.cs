caaac458783e1cc87d4f960706c32d05e611fa24
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2024-04-09 00:46:31 -0300

    Actividad1

---
+++ Técnicas Avanzadas de Programación/Clase7/Actividad1/Actividad1/Cliente.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad1
+{
+    class Cliente
+    {
+        // Datos miembros protegidos
+        protected long NumeroCuenta;
+        protected string Apellido;
+        protected string Nombre;
+        protected string Direccion;
+        protected string FechaNac;
+        protected double Saldo;
+
+        // Constructor
+        public Cliente(long numeroCuenta, string apellido, string nombre, string direccion, string fechaNac, double saldoInicial)
+        {
+            NumeroCuenta = numeroCuenta;
+            Apellido = apellido;
+            Nombre = nombre;
+            Direccion = direccion;
+            FechaNac = fechaNac;
+            Saldo = saldoInicial;
+        }
+
+        // Funciones miembro públicas
+        public void PonerApellido(string apellido)
+        {
+            Apellido = apellido;
+        }
+
+        public string ObtenerApellido()
+        {
+            return Apellido;
+        }
+
+        public void PonerNombre(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public string ObtenerNombre()
+        {
+            return Nombre;
+        }
+
+        public void PonerDireccion(string direccion)
+        {
+            Direccion = direccion;
+        }
+
+        public string ObtenerDireccion()
+        {
+            return Direccion;
+        }
+
+        public void PonerFechaNac(string fechaNac)
+        {
+            FechaNac = fechaNac;
+        }
+
+        public string ObtenerFechaNac()
+        {
+            return FechaNac;
+        }
+
+        public double ObtenerSaldo()
+        {
+            return Saldo;
+        }
+
+        public double Depositar(double monto)
+        {
+            Saldo += monto;
+            return Saldo;
+        }
+
+        public double Retirar(double monto)
+        {
+            if (Saldo >= monto)
+            {
+                Saldo -= monto;
+                return Saldo;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ Técnicas Avanzadas de Programación/Clase7/Actividad1/Actividad1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Actividad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejemplo de uso
+            Cliente cliente1 = new Cliente(123456789, "Varg", "Abigail", "Calle Falsa 123", "11/07/1992", 1500.0);
+
+            Console.WriteLine("Cliente: " + cliente1.ObtenerNombre() + " " + cliente1.ObtenerApellido());
+            Console.WriteLine("Dirección: " + cliente1.ObtenerDireccion());
+            Console.WriteLine("Fecha de Nacimiento: " + cliente1.ObtenerFechaNac());
+            Console.WriteLine("Saldo inicial: $" + cliente1.ObtenerSaldo());
+
+            // Depositar $500
+            cliente1.Depositar(500);
+            Console.WriteLine("Saldo después de depositar $500: $" + cliente1.ObtenerSaldo());
+
+            // Retirar $200 (saldo suficiente)
+            double nuevoSaldo = cliente1.Retirar(200);
+            if (nuevoSaldo != -1)
+            {
+                Console.WriteLine("Saldo después de retirar $200: $" + nuevoSaldo);
+            }
+            else
+            {
+                Console.WriteLine("No hay suficiente saldo para retirar $200.");
+            }
+
+            // Intentar retirar $1500 (saldo insuficiente)
+            nuevoSaldo = cliente1.Retirar(1500);
+            if (nuevoSaldo != -1)
+            {
+                Console.WriteLine("Saldo después de retirar $1500: $" + nuevoSaldo);
+            }
+            else
+            {
+                Console.WriteLine("No hay suficiente saldo para retirar $1500.");
+            }
+        }
+    }
+}
